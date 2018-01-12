d9415fc07a6ab68c2de5896d60d6837b956bfa26
ArticCynda <yannick.verbelen@gmail.com>
2018-01-13 00:54:46 +0100

    removed travis

--- discharger/embedded/Uno/src/main.cpp
+++ discharger/embedded/Uno/src/main.cpp
@@ -1,13 +1,19 @@
 #include <Arduino.h>
+#include <TimerOne.h>
 
 #define MosEnable (2)
 #define BatADC    (A0)
+
+#define t_ON_ms   1000
+#define t_OFF_ms  5000
 
 void setup() {
     // put your setup code here, to run once:
 
     pinMode(MosEnable, OUTPUT);
     pinMode(BatADC, INPUT);
+
+
 
 }
 
